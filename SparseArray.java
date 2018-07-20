51e987cde9dc4c22ea8f128dff2f03adad7bda3d
Akshansh Ohm <akshanshohm1999@gmail.com>
2018-07-20 13:08:08 +0530

    Create SparseArray.java

---
+++ SparseArray.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the matchingStrings function below.
+    static int[] matchingStrings(String[] strings, String[] queries) {
+        int i,j;
+        int A= strings.length;
+        int B= queries.length;
+        int count[]=new int[B];
+        for(i=0;i<A;i++){
+        for(j=0;j<B;j++){
+           // count[j]=0;
+          if(strings[i].compareTo(queries[j])==0){
+              count[j]=count[j]+1;
+          }
+        }
+    }
+return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] strings = new String[stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            String stringsItem = scanner.nextLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String[] queries = new String[queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            String queriesItem = scanner.nextLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
